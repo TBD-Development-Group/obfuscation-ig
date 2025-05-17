1a57add30e7ee1df4394204d6575a71ce1553c12
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-17 17:19:51 +0200

    Update bytecodeBuilder.js

--- LuaU-Obfuscator/src/bytecodeBuilder.js
+++ LuaU-Obfuscator/src/bytecodeBuilder.js
@@ -1,1 +1,31 @@
+// Convert AST into our custom bytecode format
+
+const OPCODES = {
+  LOADK: 1,
+  ADD: 2,
+  SUB: 3,
+  MUL: 4,
+  DIV: 5,
+  PRINT: 6,
+  JMP: 7,
+  EQ: 8,
+  LT: 9,
+  CALL: 10,
+  RET: 11,
+  // ... add more opcodes as needed
+};
+
+function buildBytecode(ast) {
+  const bytecode = [];
+  // Walk AST and produce opcode arrays like:
+  // [OPCODES.LOADK, reg, constant]
+  // [OPCODES.PRINT, reg]
+  // (Implementation detail left for expansion)
+  return bytecode;
+}
+
+module.exports = {
+  OPCODES,
+  buildBytecode,
+};
 
