4302a24c72841f6000f4bae1286bfba769daec0b
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-17 17:26:38 +0200

    Update vmTemplate.lua

--- LuaU-Obfuscator/src/vmTemplate.lua
+++ LuaU-Obfuscator/src/vmTemplate.lua
@@ -1,45 +1,50 @@
--- Custom LuaU VM Interpreter
+-- Custom Lua VM interpreter for obfuscated bytecode
 
 local OPCODES = {
   LOADK = 1,
   ADD = 2,
-  SUB = 3,
-  MUL = 4,
-  DIV = 5,
-  PRINT = 6,
-  JMP = 7,
-  EQ = 8,
-  LT = 9,
-  CALL = 10,
-  RET = 11,
+  PRINT = 3,
+  RET = 4,
 }
 
-local code = /*BYTECODE_PLACEHOLDER*/
+local code = /*BYTECODE_PLACEHOLDER*/.bytecode
+local constants = /*BYTECODE_PLACEHOLDER*/.constants
+
 local pc = 1
-local stack = {}
-local running = true
+local registers = {}
 
-local function run()
-  while running do
-    local ins = code[pc]
-    local op = ins[1]
-
-    if op == OPCODES.LOADK then
-      local reg, val = ins[2], ins[3]
-      stack[reg] = val
-    elseif op == OPCODES.PRINT then
-      print(stack[ins[2]])
-    elseif op == OPCODES.RET then
-      running = false
-    else
-      error("Unknown opcode: " .. tostring(op))
-    end
-    pc = pc + 1
-  end
+-- Decode hex string to normal string
+local function hexToString(hex)
+  return (hex:gsub('..', function(cc)
+    return string.char(tonumber(cc, 16))
+  end))
 end
 
--- Anti-Tamper snippet injected here
-/*CHECKSUM_PLACEHOLDER*/
+local running = true
+while running do
+  local ins = code[pc]
+  local op = ins[1]
 
-run()
+  if op == OPCODES.LOADK then
+    local reg = ins[2]
+    local constIndex = ins[3] + 1 -- Lua index starts at 1
+    local val = constants[constIndex]
+    -- Decode if hex string
+    if type(val) == 'string' and val:match('^%x+$') then
+      val = hexToString(val)
+    end
+    registers[reg] = val
 
+  elseif op == OPCODES.PRINT then
+    local reg = ins[2]
+    print(registers[reg])
+
+  elseif op == OPCODES.RET then
+    running = false
+
+  else
+    error("Unknown opcode " .. tostring(op))
+  end
+
+  pc = pc + 1
+end
