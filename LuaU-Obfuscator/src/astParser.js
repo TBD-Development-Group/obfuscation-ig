b5c93aaf5501e442756dd46aad86e74fecd7820d
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-17 17:23:23 +0200

    Update astParser.js

--- LuaU-Obfuscator/src/astParser.js
+++ LuaU-Obfuscator/src/astParser.js
@@ -1,1 +1,8 @@
+const luaparse = require('luaparse');
+
+function parseLuaToAST(luaCode) {
+  return luaparse.parse(luaCode, { luaVersion: '5.1', comments: false, locations: false });
+}
+
+module.exports = { parseLuaToAST };
 
